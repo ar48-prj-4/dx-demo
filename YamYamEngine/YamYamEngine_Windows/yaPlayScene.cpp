5c6ea9eaafd0befb87d5dcc4f10a7dc9aa9e6f38
Jeong-Hayoon <hayn017@gmail.com>
2023-10-01 17:39:41 +0900

    오타 수정

--- YamYamEngine/YamYamEngine_Windows/yaPlayScene.cpp
+++ YamYamEngine/YamYamEngine_Windows/yaPlayScene.cpp
@@ -28,15 +28,27 @@ namespace ya
 		Player* player = new Player();
 
 		Transform* tr = player->AddComponent<Transform>();
-		tr->SetPosition(Vector3(0.5f, 0.2f, 0.0f));
-		tr->SetScale(Vector3(1.0f, 1.0f, 1.0f));
+		tr->SetPosition(Vector3(0.8f, 0.2f, 0.0f));
+		tr->SetScale(Vector3(0.5f, 0.5f, 1.0f));
 
 		MeshRenderer* meshRenderer = player->AddComponent<MeshRenderer>();
 		meshRenderer->SetMesh(Resources::Find<Mesh>(L"TriangleMesh"));
 		meshRenderer->SetShader(Resources::Find<Shader>(L"TriangleShader"));
 
-		AddGameObject(player, LAYER::NONE);
+		AddGameObject(player, LAYER::PLAYER);
 		player->AddComponent<PlayerScript>();
+
+		Player* player2 = new Player();
+
+		Transform* tr2 = player2->AddComponent<Transform>();
+		tr2->SetPosition(Vector3(0.2f, 0.2f, 0.0f));
+		tr2->SetScale(Vector3(0.5f, 0.5f, 1.0f));
+
+		MeshRenderer* meshRenderer2 = player2->AddComponent<MeshRenderer>();
+		meshRenderer2->SetMesh(Resources::Find<Mesh>(L"TriangleMesh"));
+		meshRenderer2->SetShader(Resources::Find<Shader>(L"TriangleShader"));
+
+		AddGameObject(player2, LAYER::PLAYER);
 	}
 
 	void PlayScene::Update()
