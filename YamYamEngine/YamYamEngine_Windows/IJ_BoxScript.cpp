b7815cf43dc9042e0ddd6c06d2d1405df5647c20
ILJUN-SEO <iljun.seo0177@gmail.com>
2023-10-02 16:37:58 +0900

    리뷰를 참고하여 수정

--- YamYamEngine/YamYamEngine_Windows/IJ_BoxScript.cpp
+++ YamYamEngine/YamYamEngine_Windows/IJ_BoxScript.cpp
@@ -85,12 +85,13 @@ namespace IJ
 			ya::Transform* tr_Player = m_Grabber->GetComponent<ya::Transform>();
 			ya::Vector3 pos_Player = tr_Player->GetPosition();
 
-			float m_Distance = sqrtf(powf(pos_Box.x - pos_Player.x, 2) +
-								powf(pos_Box.y - pos_Player.y, 2));
+			//float m_Distance = sqrtf(powf(pos_Box.x - pos_Player.x, 2) +
+			//					powf(pos_Box.y - pos_Player.y, 2));
+			float m_Distance = ya::Vector3::DistanceSquared(pos_Box, pos_Player);
 
 			// 나중에 구현 예정
 			// 무언가에 끼어서 움직이지 못해 나를 잡은 플레이어와 거리가 멀어지면 idle로 돌아감
-			//if (fabsf(m_Distance) > 0.5f)
+			//if (m_Distance > 0.5f)
 			//{
 			//	m_Owner_Box->SetGrabber(nullptr);
 			//}
@@ -105,7 +106,7 @@ namespace IJ
 
 			// 또한 중력을 상쇄하기 위해 중력의 크기만큼 위로 힘을 받습니다
 			// 일단은 기본값인 800.0f 만큼만 힘을 줍니다
-			rigidbody->AddForce({ 0.0f, -800.0f });
+			rigidbody->AddForce({ 0.0f, -800.0f * ya::Time::DeltaTime()});
 		}
 	}
 }
